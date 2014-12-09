403d6ce59abb47033f0332d45159354b8ece36c2
Maxime Vidori <maxime.vidori@gmail.com>
2014-12-09 14:07:30 +0100

    Creation of database scripts

--- create_tables.sql
+++ create_tables.sql
@@ -1,3 +1,53 @@
-CREATE TABLE recipes (
-  name            varchar(80)
+CREATE SCHEMA IF NOT EXISTS rulzurkitchen;
+
+-- Creation of enum types
+DROP TYPE IF EXISTS rulzurkitchen.measurement;
+DROP TYPE IF EXISTS rulzurkitchen.type;
+DROP TYPE IF EXISTS rulzurkitchen.duration;
+
+CREATE TYPE rulzurkitchen.measurement AS ENUM ('L', 'g', 'oz', 'spoon');
+CREATE TYPE rulzurkitchen.type AS ENUM ('starter', 'main', 'dessert');
+CREATE TYPE rulzurkitchen.duration AS ENUM('0/5', '5/10', '10/15', '15/20',
+  '20/25', '25/30', '30/45', '45/60', '60/75', '75/90', '90/120', '120/150');
+
+
+-- Creation of tables
+CREATE TABLE IF NOT EXISTS rulzurkitchen.ingredient (
+    id SERIAL PRIMARY KEY,
+    name varchar(20)
 );
+
+CREATE TABLE IF NOT EXISTS rulzurkitchen.utensil (
+    id serial PRIMARY KEY,
+    name varchar(20)
+);
+
+CREATE TABLE IF NOT EXISTS rulzurkitchen.recipe (
+    id serial PRIMARY KEY,
+    name varchar(20),
+    directions json,
+    difficulty smallint CONSTRAINT difficulty_borders CHECK
+      (difficulty > 0 AND difficulty < 6),
+    duration rulzurkitchen.duration,
+    people smallint CONSTRAINT people_borders CHECK
+      (people > 0 AND people < 13),
+    type rulzurkitchen.type
+);
+
+CREATE TABLE IF NOT EXISTS rulzurkitchen.recipe_ingredients (
+    fk_recipe integer REFERENCES rulzurkitchen.recipe (id),
+    fk_ingredient integer REFERENCES rulzurkitchen.ingredient (id),
+    quantity smallint,
+    measurement rulzurkitchen.measurement,
+    PRIMARY KEY (fk_recipe, fk_ingredient)
+);
+
+CREATE TABLE IF NOT EXISTS rulzurkitchen.recipe_utensils (
+    fk_recipe integer REFERENCES rulzurkitchen.recipe (id),
+    fk_utensil integer REFERENCES rulzurkitchen.utensil (id),
+    PRIMARY KEY (fk_recipe, fk_utensil)
+);
+
+
+
+
---
+++ delete_all.sql
@@ -0,0 +1,1 @@
+DROP SCHEMA rulzurkitchen CASCADE;
