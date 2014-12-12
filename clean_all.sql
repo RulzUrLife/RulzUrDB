d666778ffdec7279d590d45a9ef8d307474dc86e
Maxime Vidori <maxime.vidori@gmail.com>
2014-12-12 10:53:17 +0100

    Add a script to clean tables

---
+++ clean_all.sql
@@ -0,0 +1,5 @@
+DELETE FROM rulzurkitchen.recipe_utensils;
+DELETE FROM rulzurkitchen.recipe_ingredients;
+DELETE FROM rulzurkitchen.recipe;
+DELETE FROM rulzurkitchen.utensil;
+DELETE FROM rulzurkitchen.ingredient;
